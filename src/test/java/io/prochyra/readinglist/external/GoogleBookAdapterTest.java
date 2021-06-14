ee7864cc373ea6d899fc7accf154f193912d9769
Andrzej Prochyra <andrzej@prochyra.name>
2021-06-14 11:14:46 +0100

    Refactor tests for clarity

--- src/test/java/io/prochyra/readinglist/external/GoogleBookAdapterTest.java
+++ src/test/java/io/prochyra/readinglist/external/GoogleBookAdapterTest.java
@@ -4,33 +4,50 @@ import io.prochyra.readinglist.domain.Book;
 import jakarta.json.JsonObject;
 import jakarta.json.bind.Jsonb;
 import jakarta.json.bind.JsonbBuilder;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
+import static java.util.Collections.emptyList;
 import static java.util.List.of;
 import static org.assertj.core.api.BDDAssertions.then;
 
 @DisplayNameGeneration(ReplaceUnderscores.class)
 class GoogleBookAdapterTest {
 
+    private GoogleBookAdapter adapter;
+    private Jsonb jsonb;
+
+    private JsonObject getJsonObjectFrom(String s) {
+        return jsonb.fromJson(s, JsonObject.class);
+    }
+
+    private ArrayList<Book> getList(Book... book) {
+        return new ArrayList<>(Arrays.asList(book));
+    }
+
+    @BeforeEach
+    void setUp() {
+        adapter = new GoogleBookAdapter();
+        jsonb = JsonbBuilder.create();
+    }
+
     @Test
     void should_adapt_empty_json_object_to_empty_list() {
-        GoogleBookAdapter adapter = new GoogleBookAdapter();
-        Jsonb jsonb = JsonbBuilder.create();
-        String emptyJsonObject = "{}";
-        JsonObject json = jsonb.fromJson(emptyJsonObject, JsonObject.class);
+        var json = getJsonObjectFrom("{}");
 
-        then(adapter.adaptFromJson(json)).isEmpty();
+        var books = adapter.adaptFromJson(json);
+
+        then(books).isEmpty();
     }
 
     @Test
     void should_adapt_book_with_only_a_title() {
-        GoogleBookAdapter adapter = new GoogleBookAdapter();
-        Jsonb jsonb = JsonbBuilder.create();
-        String onlyTitle = """
+        var json = getJsonObjectFrom("""
                 {
                   "items": [
                     {
@@ -40,20 +57,18 @@ class GoogleBookAdapterTest {
                     }
                   ]
                 }
-                """;
+                """);
+        var expectedBooks = getList(
+                new Book("Title", emptyList(), "UNKNOWN"));
 
-        JsonObject json = jsonb.fromJson(onlyTitle, JsonObject.class);
+        var books = adapter.adaptFromJson(json);
 
-        ArrayList<Book> expectedBooks = new ArrayList<>(of(new Book("Title", of(), "UNKNOWN")));
-
-        then(adapter.adaptFromJson(json)).isEqualTo(expectedBooks);
+        then(books).isEqualTo(expectedBooks);
     }
 
     @Test
     void should_adapt_a_book_with_multiple_authors() {
-        GoogleBookAdapter adapter = new GoogleBookAdapter();
-        Jsonb jsonb = JsonbBuilder.create();
-        String multipleAuthors = """
+        JsonObject json = getJsonObjectFrom("""
                 {
                   "items": [
                     {
@@ -67,14 +82,12 @@ class GoogleBookAdapterTest {
                       }
                     }
                   ]
-                }""";
+                }""");
+        var expectedBooks = getList(
+                new Book("Title", of("Author One", "Author Two"), "A Publisher"));
 
+        ArrayList<Book> books = adapter.adaptFromJson(json);
 
-        JsonObject json = jsonb.fromJson(multipleAuthors, JsonObject.class);
-
-        ArrayList<Book> expectedBooks = new ArrayList<>(
-                of(new Book("Title", of("Author One", "Author Two"), "A Publisher")));
-
-        then(adapter.adaptFromJson(json)).isEqualTo(expectedBooks);
+        then(books).isEqualTo(expectedBooks);
     }
 }
