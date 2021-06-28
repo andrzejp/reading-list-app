1fe4b20c631735875e4ffcd31ab8dfbef6f299a6
Andrzej Prochyra <andrzej@prochyra.name>
2021-06-28 14:38:12 +0100

    Make Book a record

--- src/main/java/io/prochyra/readinglist/domain/Book.java
+++ src/main/java/io/prochyra/readinglist/domain/Book.java
@@ -1,38 +1,14 @@
 package io.prochyra.readinglist.domain;
 
 import java.util.List;
-import java.util.Objects;
 
 import static java.lang.String.format;
 import static java.lang.String.join;
 
-public class Book {
-
-    private final String title;
-    private final List<String> authors;
-    private final String publisher;
-
-    public Book(String title, List<String> authors, String publisher) {
-        this.title = title;
-        this.authors = authors;
-        this.publisher = publisher;
-    }
+public record Book(String title, List<String> authors, String publisher) {
 
     @Override
     public String toString() {
         return format("'%s' by %s - %s", title, join(", ", authors), publisher);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        var book = (Book) o;
-        return title.equals(book.title) && authors.equals(book.authors) && publisher.equals(book.publisher);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(title, authors, publisher);
     }
 }
